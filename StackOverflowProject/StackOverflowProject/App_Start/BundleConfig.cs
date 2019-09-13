e0835846a119dabd69f9924573a660e8494d9d59
unknown <shahedkaiser@hotmail.com>
2019-09-13 20:21:01 +0600

    First Commit

---
+++ StackOverflowProject/StackOverflowProject/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StackOverflowProject
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include("~/scripts/jquery-3.3.1.js", "~/scripts/umd/popper.js", "~/scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/styles/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/styles/site").Include("~/Content/styles.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ StackOverflowProject/StackOverflowProject/Controllers/QuestionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StackOverflowProject.ViewModels;
+using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.CustomFilters;
+
+namespace StackOverflowProject.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        IQuestionsService qs;
+        IAnswersService asr;
+        ICategoriesService cs;
+
+        public QuestionsController(IQuestionsService qs, IAnswersService asr, ICategoriesService cs)
+        {
+            this.qs = qs;
+            this.asr = asr;
+            this.cs = cs;
+        }
+
+        // GET: Questions
+        public ActionResult View(int id)
+        {
+            this.qs.UpdateQuestionViewsCount(id, 1);
+            int uid = Convert.ToInt32(Session["CurrentUserID"]);
+            QuestionViewModel qvm = this.qs.GetQuestionByQuestionID(id, uid);
+            return View(qvm);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [UserAuthorizationFilter]
+        public ActionResult AddAnswer(NewAnswerViewModel navm)
+        {
+            navm.UserID= Convert.ToInt32(Session["CurrentUserID"]);
+            navm.AnswerDateAndTime = DateTime.Now;
+            navm.VotesCount = 0;
+
+            if(ModelState.IsValid)
+            {
+                this.asr.InsertAnswer(navm);
+                return RedirectToAction("View","Questions", new { id=navm.QuestionID});
+            }
+
+            else
+            {
+                ModelState.AddModelError("x", "Invalid data");
+                QuestionViewModel qvm = this.qs.GetQuestionByQuestionID(navm.QuestionID,navm.UserID);
+                return View("View", qvm);
+            }
+        }
+
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [UserAuthorizationFilter]
+        public ActionResult EditAnswer(EditAnswerViewModel avm)
+        {
+            if(ModelState.IsValid)
+            {
+                avm.UserID = Convert.ToInt32(Session["CurrentUserID"]);
+                this.asr.UpdateAnswer(avm);
+                return RedirectToAction("View", "Questions", new { id = avm.QuestionID });
+            }
+            else
+            {
+                ModelState.AddModelError("x", "Invalid data");
+                return RedirectToAction("View", "Questions", new { id = avm.QuestionID });
+            }
+           
+        }
+
+
+        public ActionResult Create()
+        {
+            List<CategoryViewModel> categories = this.cs.GetCategories();
+            ViewBag.categories = categories;
+            return View();
+        }
+
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        [UserAuthorizationFilter]
+        public ActionResult Create(NewQuestionViewModel qvm)
+        {
+            if(ModelState.IsValid)
+            {
+                qvm.UserID= Convert.ToInt32(Session["CurrentUserID"]);
+                qvm.AnswersCount = 0;
+                qvm.ViewsCount = 0;
+                qvm.VotesCount = 0;
+                qvm.QuestionDateAndTime = DateTime.Now;
+                this.qs.InsertQuestion(qvm);
+                return RedirectToAction("Questions", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("x", "Invalid data");
+                return View();
+            }
+        }
+    }
+}
